3037fdf84fb495c91b2706d846768571dd30d827
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-02-15 12:32:35 +0300

    Made AccountServerControllerMBean interface

--- src/main/java/accountServer/AccountServerControllerMBean.java
+++ src/main/java/accountServer/AccountServerControllerMBean.java
@@ -3,5 +3,12 @@ package accountServer;
 /**
  * Created by User on 15.02.2016.
  */
+
+@SuppressWarnings("UnusedDeclaration")
 public interface AccountServerControllerMBean {
+    public int getUsers();
+
+    public int getUsersLimit();
+
+    public void setUsersLimit(int usersLimit);
 }
