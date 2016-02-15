ece0679605acebc84bd0c2988f58b0f2adb3f877
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-02-15 12:46:20 +0300

    Realized AccountServer class

--- src/main/java/accountServer/AccountServer.java
+++ src/main/java/accountServer/AccountServer.java
@@ -3,5 +3,37 @@ package accountServer;
 /**
  * Created by User on 15.02.2016.
  */
-public class AccountServer {
+public class AccountServer implements AccountServerI {
+    private int usersCount;
+    private int usersLimit;
+
+    public AccountServer(int usersLimit){
+        this.usersCount = 0;
+        this.usersLimit = usersLimit;
+    }
+
+    @Override
+    public void addNewUser(){
+        usersCount++;
+    }
+
+    @Override
+    public void removeUser(){
+        usersCount--;
+    }
+
+    @Override
+    public int getUsersLimit(){
+        return usersLimit;
+    }
+
+    @Override
+    public int getUsersCount(){
+        return usersCount;
+    }
+
+    @Override
+    public void setUsersLimit(int usersLimit){
+        this.usersLimit = usersLimit;
+    }
 }
