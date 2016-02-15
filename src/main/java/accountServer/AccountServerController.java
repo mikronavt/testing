aefb2eeefe72c3297eeaf597f7a87652e9053dd6
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-02-15 12:41:19 +0300

    Realized AccountServerController class

--- src/main/java/accountServer/AccountServerController.java
+++ src/main/java/accountServer/AccountServerController.java
@@ -3,5 +3,26 @@ package accountServer;
 /**
  * Created by User on 15.02.2016.
  */
-public class AccountServerController {
+public class AccountServerController implements AccountServerControllerMBean {
+
+    private final AccountServerI accountServer;
+
+    public AccountServerController(AccountServerI accountServer){
+        this.accountServer = accountServer;
+    }
+
+    @Override
+    public int getUsers(){
+        return accountServer.getUsersCount();
+    }
+
+    @Override
+    public int getUsersLimit(){
+        return accountServer.getUsersLimit();
+    }
+
+    @Override
+    public void setUsersLimit(int limit){
+        accountServer.setUsersLimit(limit);
+    }
 }
