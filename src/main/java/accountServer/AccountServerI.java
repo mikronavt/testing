a46146d76feec3aec9944f09593269e5a58141f7
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-02-15 12:28:35 +0300

    Made AccountServerI interface

--- src/main/java/accountServer/AccountServerI.java
+++ src/main/java/accountServer/AccountServerI.java
@@ -4,4 +4,13 @@ package accountServer;
  * Created by User on 15.02.2016.
  */
 public interface AccountServerI {
+    void addNewUser();
+
+    void removeUser();
+
+    int getUsersLimit();
+
+    void setUsersLimit(int usersLimit);
+
+    int getUsersCount();
 }
